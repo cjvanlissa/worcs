454f622bc0cfe52db7168469aa72003dfda1989b
Barbara Vreede <b.vreede@gmail.com>
2020-03-27 09:05:22 +0100

    fixed typo in welcome message

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 # @importFrom rstudioapi versionInfo
 .onAttach <- function(libname, pkgname) {
   #correct_version <- versionInfo()$version >= "1.1.28"
-  print_message <- paste0("Welcome to WORCS: Worflow for Open Reproducible Code in Science. Please read the tutorial before using this package, and consider citing it. ",
+  print_message <- paste0("Welcome to WORCS: Workflow for Open Reproducible Code in Science. Please read the tutorial before using this package, and consider citing it. ",
                           ifelse(has_git(), "",
                                  "Rstudio is not yet connected to Git. You will not be able to use the worcs package yet.")
                           #, ifelse(correct_version, "Haaai", "Rstudio version 1.1.28 or higher is required to use the worcs package.")
