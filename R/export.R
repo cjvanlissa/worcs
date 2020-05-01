84efa24714abe37d2e0088a2729b8fbfc36141b2
Brandmaier <brandmaier@mpib-berlin.mpg.de>
2020-05-01 17:27:56 +0200

    added functionality to export

--- R/export.R
+++ R/export.R
@@ -5,20 +5,28 @@
 #' @return boolean. Success of export operation.
 #' @export
 export_project <- function(filename, open.data=TRUE){
+
   if (!is.character(filename)) {
     print("Filename must be of type character.")
     invisible(FALSE)
   } 
   
-  if (file.exists(filename)) {
-    print("Error! File exists!")
-    invisible(FALSE)
-  }
+  # TODO: is there a robust way to get Rstudio base path?
+  base.dir <- getwd()
+  project.folder <- tail(strsplit(base.dir,.Platform$file.sep)[[1]],1)
+  zip.mode <- endsWith(filename,".zip")
+
+  browser()
+
   
-  
-  
-  if (endsWith(filename,".zip")) {
-    files <- list.files(getwd(),full.names = TRUE)
+  if (zip.mode) {
+    
+    if (file.exists(filename)) {
+      print("Error! File exists!")
+      invisible(FALSE)
+    }
+    
+    files <- list.files(getwd(),full.names = FALSE)
     
     if (isFALSE(open.data)) {
       filter <- sapply(files, function(x){!endsWith(x,"data.csv")})
@@ -28,11 +36,31 @@ export_project <- function(filename, open.data=TRUE){
       files <- files[filter]
     }
     
-    utils::zip(filename, files=files, flags="-r")
+    utils::zip(filename, files=files, flags="-rq",)
+    
   } else {
-    if (!file.exists(filename)) {
-      dir.create(filename, recursive=TRUE)
+    
+    if (file.exists(paste0(filename,.Platform$file.sep,project.folder))) {
+      print("Error! Target directory exists!")
+      invisible(FALSE)
     }
+    
+    files <- list.files(base.dir,full.names = TRUE)
+    
+    dir.create(filename, recursive=TRUE)
+    file.copy(paste0(base.dir,.Platform$file.sep), 
+              filename, 
+              recursive = TRUE,
+              copy.mode = TRUE,
+              copy.date = TRUE)
+    
+    # remove data file
+    if (isFALSE(open.data)) {
+
+      file.remove(file.path(filename,.Platform$file.sep,project.folder,"data.csv"))
+      print("Excluding open data file.")
+    }
+    
   }
   
   invisible(TRUE)
