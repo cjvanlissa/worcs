e044feb7d74fbbc4e8bd5e68c4fd93650d6722ee
Brandmaier <brandmaier@mpib-berlin.mpg.de>
2020-05-01 18:30:35 +0200

    bugfix

--- R/export.R
+++ R/export.R
@@ -1,29 +1,22 @@
 
-#' title Export Project
-#' @param filename path to a directory or a file
-#' @param open.data boolean. include open data?
-#' @return boolean. Success of export operation.
-#' @export
-export_project <- function(filename, open.data=TRUE){
-
+export_project <- function(filename, open.data=TRUE)
+{
+  
   if (!is.character(filename)) {
     print("Filename must be of type character.")
-    invisible(FALSE)
+    return(FALSE)
   } 
   
   # TODO: is there a robust way to get Rstudio base path?
   base.dir <- getwd()
   project.folder <- tail(strsplit(base.dir,.Platform$file.sep)[[1]],1)
   zip.mode <- endsWith(filename,".zip")
-
-  browser()
-
   
   if (zip.mode) {
     
     if (file.exists(filename)) {
       print("Error! File exists!")
-      invisible(FALSE)
+      return(FALSE)
     }
     
     files <- list.files(getwd(),full.names = FALSE)
@@ -38,11 +31,12 @@ export_project <- function(filename, open.data=TRUE){
     
     utils::zip(filename, files=files, flags="-rq",)
     
-  } else {
+  } 
+  else {
     
     if (file.exists(paste0(filename,.Platform$file.sep,project.folder))) {
       print("Error! Target directory exists!")
-      invisible(FALSE)
+      return(FALSE)
     }
     
     files <- list.files(base.dir,full.names = TRUE)
@@ -56,12 +50,9 @@ export_project <- function(filename, open.data=TRUE){
     
     # remove data file
     if (isFALSE(open.data)) {
-
+      
       file.remove(file.path(filename,.Platform$file.sep,project.folder,"data.csv"))
       print("Excluding open data file.")
     }
-    
   }
-  
-  invisible(TRUE)
 }
