1c499d4e17ad400080c006218d765f659e1d3541
cjvanlissa <c.j.vanlissa@uu.nl>
2020-05-05 10:24:34 +0200

    Finalize export_project

--- R/export.R
+++ R/export.R
@@ -1,76 +1,62 @@
-#' @title Export Project
-#' @param filename path to a target directory or path to a zip file that is to be created
-#' @param open.data boolean. Export including open data?
-#' @return boolean. Success of the operation
-#' @importFrom utils tail
+#' @title Export project to .zip file
+#' @param filename Character. Path to a \code{.zip} file that is to be created.
+#' The default argument \code{NULL} creates a \code{.zip} file in the directory
+#' one level above the 'worcs' project directory. By default, all files tracked
+#' by 'Git' are included in the \code{.zip} file, excluding 'data.csv' if
+#' \code{open_data = FALSE}.
+#' @param open_data Logical. Whether or not to include the original data,
+#' 'data.csv', if this file exists. If \code{open_data = FALSE} and an open
+#' data file does exist, then it is excluded from the \code{.zip} file. If it
+#' does not yet exist, a synthetic data set is generated and added to the
+#' \code{.zip} file.
+#' @return Logical, indicating the success of the operation. This function is
+#' called for its side effect of creating a \code{.zip} file.
+#' @importFrom utils tail zip
 #' @importFrom gert git_ls
 #' @export
-export_project <- function(filename=NULL, open.data=TRUE)
+export_project <- function(filename = NULL, open_data = TRUE)
 {
   # get properties about the project and paths
-  # TODO: is there a robust way to get Rstudio base path?
-  #  base.dir <- rstudioapi::getActiveProject()
-  base.dir <- getwd()
-  # List tracked files: gert::git_ls()
-  project.folder <- tail(strsplit(base.dir,.Platform$file.sep)[[1]],1)
+  base_dir <- normalizePath(".")
+
+  project_folder <- gsub("^.+\\b(.+?)$", "\\1", base_dir)
 
   # if no filename is given, export to a zip file with
   # the name of the project folder
   if (is.null(filename)) {
-    filename=paste0("..",.Platform$file.sep,project.folder,".zip")
+    filename <- file.path("..", paste0(project_folder, ".zip"))
   }
 
   if (!is.character(filename)) {
-    message("Filename must be of type character:",as.character(filename))
+    message("Filename must be of type character: ",as.character(filename))
     return(FALSE)
   }
 
-  # determine mode of operation
-  zip.mode <- endsWith(filename,".zip")
-
-
-  if (zip.mode) {
 
     if (file.exists(filename)) {
-      message("Error! File exists!")
+      stop("Could not write to '", filename, "' because the file already exists.")
       return(FALSE)
     }
     # Use this to decide which files to ZIP, but always add data.csv
-    # if the user specifies open.data = TRUE
-    files <- git_ls()
-
-    if (isFALSE(open.data)) {
-      filter <- sapply(files, function(x){!endsWith(x,"data.csv")})
-      if (any(filter)) {
-        print("Excluding open data file.")
+    # if the user specifies open_data = TRUE
+    zip_these <- git_ls()$path
+    tmpfile <- NULL
+    if (isFALSE(open_data)) {
+      hasdata <- zip_these == "data.csv"
+      if (any(hasdata)) {
+        message("Excluding open data file and generating synthetic data for archive. Ensure that no identifying information is included.")
+        zip_these <- zip_these[!hasdata]
+        if(!any(zip_these == "synthetic_data.csv")){
+          synth <- synthetic(data, verbose = FALSE)
+          tmpfile <- file.path(tempdir(), "synthetic_data.csv")
+          write.csv(synth$syn, tmpfile, row.names = FALSE)
+          zip_these <- c(zip_these, tmpfile)
+        }
       }
-      files <- files[filter]
     }
-
-    utils::zip(filename, files=files, flags="-rq",)
-
-  }
-  else {
-
-    if (file.exists(paste0(filename,.Platform$file.sep,project.folder))) {
-      print("Error! Target directory exists!")
-      return(FALSE)
+    zip(filename, files = zip_these, flags="-rq")
+    if(!is.null(tmpfile)){
+      invisible(file.remove(tmpfile))
     }
-
-    files <- list.files(base.dir,full.names = TRUE)
-
-    dir.create(filename, recursive=TRUE)
-    file.copy(paste0(base.dir,.Platform$file.sep),
-              filename,
-              recursive = TRUE,
-              copy.mode = TRUE,
-              copy.date = TRUE)
-
-    # remove data file
-    if (isFALSE(open.data)) {
-
-      file.remove(file.path(filename,.Platform$file.sep,project.folder,"data.csv"))
-      print("Excluding open data file.")
-    }
-  }
+    return(TRUE)
 }
