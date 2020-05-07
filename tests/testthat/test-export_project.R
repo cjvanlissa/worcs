2d295419c5ef035435773d806b358ead0c72e3d0
cjvanlissa <c.j.vanlissa@uu.nl>
2020-05-07 18:53:17 +0200

    Test export only on windows

--- tests/testthat/test-export_project.R
+++ tests/testthat/test-export_project.R
@@ -3,18 +3,19 @@
 # dir.create(file.path(tempdir(), the_test))
 # setwd(file.path(tempdir(), "citeall"))
 
-get_sig <- tryCatch(git_signature_default(), error = function(e){
-  gert::git_config_global_set(name = "user.name", value = "yourname")
-  gert::git_config_global_set(name = "user.email", value = "yourname@email.com")
-})
 
-if(!Sys.info()['sysname'] == "Windows"){
-  if(Sys.getenv("R_ZIPCMD") == ""){
-    library(rtools)
-    if(devtools::find_rtools()) Sys.setenv(R_ZIPCMD= file.path(devtools:::dev_sitrep()$rtools_path, "zip"))
-  }
-}
-
+#
+# if(!Sys.info()['sysname'] == "Windows"){
+#   if(Sys.getenv("R_ZIPCMD") == ""){
+#     library(rtools)
+#     if(devtools::find_rtools()) Sys.setenv(R_ZIPCMD= file.path(devtools:::dev_sitrep()$rtools_path, "zip"))
+#   }
+# }
+if(Sys.info()['sysname'] == "Windows"){
+  get_sig <- tryCatch(git_signature_default(), error = function(e){
+    gert::git_config_global_set(name = "user.name", value = "yourname")
+    gert::git_config_global_set(name = "user.email", value = "yourname@email.com")
+  })
 suppressWarnings(
   worcs_project("export_test",
                 manuscript = "None",
@@ -28,10 +29,13 @@ setwd("export_test")
 result <- export_project(open_data = FALSE)
 setwd(old_wd)
 
-test_that("correctly exported worcs_project", {
+test_that("export returned true", {
   expect_true(result)
-  expect_true("export_test.zip" %in% list.files("../"))
 })
 
+test_that("exported worcs_project exists", {
+  expect_true("export_test.zip" %in% list.files("../"))
+})
+}
 # setwd(old_wd)
 # unlink(file.path(tempdir(), the_test))
