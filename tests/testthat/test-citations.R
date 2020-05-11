747997fd33f17ea0e263326e3ccd7b28a5979544
yourname <yourname@email.com>
2020-05-11 10:08:15 +0200

    add cite_ umlaut test #34

--- tests/testthat/test-citations.R
+++ tests/testthat/test-citations.R
@@ -28,3 +28,61 @@ test_that("documents with spaces in names can be rendered", {
   expect_true(!any(contents == "Optional reference: @reference2020"))
   expect_true(any(contents == "Optional reference:"))
 })
+
+test_that("cite_* work with umlaute", {
+
+  skip_on_cran()
+
+  the_test <- "citeall"
+  dir_name <- file.path(tempdir(), the_test)
+  file_name <- file.path(dir_name, "test.Rmd")
+  dir.create(dir_name)
+  on.exit(unlink(dir_name, recursive = TRUE), add = TRUE)
+
+  draft(file_name, template = "github_document", package = "rmarkdown",
+        create_dir = FALSE, edit = FALSE)
+
+  write(c("", "Optional räference: @@reference2020"),
+        file = file_name, append = TRUE)
+  cite_all(file_name)
+
+  contents <- readLines(gsub("Rmd$", "md", file_name))
+
+  expect_true(any(contents == "Optional räference: @reference2020"))
+
+  cite_essential(file_name)
+
+  contents <- readLines(gsub("Rmd$", "md", file_name))
+
+  expect_true(!any(contents == "Optional räference: @reference2020"))
+  expect_true(any(contents == "Optional räference:"))
+})
+
+test_that("documents with spaces in names can be rendered", {
+
+  skip_on_cran()
+
+  the_test <- "citeall"
+  dir_name <- file.path(tempdir(), the_test)
+  file_name <- file.path(dir_name, "test.Rmd")
+  dir.create(dir_name)
+  on.exit(unlink(dir_name, recursive = TRUE), add = TRUE)
+
+  draft(file_name, template = "github_document", package = "rmarkdown",
+        create_dir = FALSE, edit = FALSE)
+
+  write(c("", "Optional reference: @@reference2020"),
+        file = file_name, append = TRUE)
+  cite_all(file_name)
+
+  contents <- readLines(gsub("Rmd$", "md", file_name))
+
+  expect_true(any(contents == "Optional reference: @reference2020"))
+
+  cite_essential(file_name)
+
+  contents <- readLines(gsub("Rmd$", "md", file_name))
+
+  expect_true(!any(contents == "Optional reference: @reference2020"))
+  expect_true(any(contents == "Optional reference:"))
+})
