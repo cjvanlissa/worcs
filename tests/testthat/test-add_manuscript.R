7d49254d31adc422cd0a9cbb70b00ae3946a0f9e
cjvanlissa <c.j.vanlissa@uu.nl>
2022-07-12 06:56:23 +0200

    add tests for rticles manuscripts

--- tests/testthat/test-add_manuscript.R
+++ tests/testthat/test-add_manuscript.R
@@ -20,3 +20,49 @@ test_that("manuscript can be generated", {
   expect_true(file.exists("manuscript/references.bib"))
   setwd(old_wd)
 })
+
+test_that("rticles manuscript can be generated", {
+  library(rticles)
+  the_test <- "manuscript"
+  old_wd <- getwd()
+  test_dir <- file.path(tempdir(), the_test)
+  dir.create(test_dir)
+  setwd(test_dir)
+  file.create(file.path(tempdir(), the_test, ".worcs"))
+  on.exit(unlink(test_dir, recursive = TRUE), add = TRUE)
+
+  add_manuscript(
+    worcs_directory = ".",
+    manuscript = "plos_article",
+    remote_repo = "bla"
+  )
+  expect_true(dir.exists("manuscript"))
+  expect_true(file.exists(".worcs"))
+  #expect_true(file.exists("manuscript/american-chemical-society.csl"))
+  expect_true(file.exists("manuscript/manuscript.Rmd"))
+  expect_true(file.exists("manuscript/references.bib"))
+  setwd(old_wd)
+})
+
+test_that("papaja manuscript can be generated", {
+  library(papaja)
+  the_test <- "manuscript"
+  old_wd <- getwd()
+  test_dir <- file.path(tempdir(), the_test)
+  dir.create(test_dir)
+  setwd(test_dir)
+  file.create(file.path(tempdir(), the_test, ".worcs"))
+  on.exit(unlink(test_dir, recursive = TRUE), add = TRUE)
+
+  add_manuscript(
+    worcs_directory = ".",
+    manuscript = "APA6",
+    remote_repo = "bla"
+  )
+  expect_true(dir.exists("manuscript"))
+  expect_true(file.exists(".worcs"))
+  #expect_true(file.exists("manuscript/american-chemical-society.csl"))
+  expect_true(file.exists("manuscript/manuscript.Rmd"))
+  expect_true(file.exists("manuscript/references.bib"))
+  setwd(old_wd)
+})
