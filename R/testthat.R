e41739238ed9ba2c47d9228762fe883814836488
cjvanlissa <c.j.vanlissa@tilburguniversity.edu>
2025-07-12 11:08:23 +0200

    update 2025-07-12 11:08:23.16247

--- R/testthat.R
+++ R/testthat.R
@@ -27,3 +27,36 @@ add_testthat <- function(worcs_directory = ".", ...){
     worcs:::cli_msg("i" = "Run {.fn worcs::github_action_testthat} to add a GitHub action that evaluates the integration tests.")
   })
 }
+
+#' @title Run all tests in a WORCS Project
+#' @description As `testthat` is primarily geared towards integration tests for
+#' 'R' packages, this function conducts `testthat` test for WORCS projects.
+#' @param worcs_directory PARAM_DESCRIPTION, Default: '.'
+#' @param ... Additional arguments passed to other functions.
+#' @return No return value. This function is called for its side effects.
+#' @examples
+#' if(requireNamespace("withr", quietly = TRUE) &
+#'   requireNamespace("testthat", quietly = TRUE)){
+#'   tmpdr <- file.path(tempdir(), "testworcs")
+#'   usethis::create_project(path = tmpdr, rstudio = FALSE, open = FALSE)
+#'   usethis::with_project(tmpdr, {
+#'   writeLines("", ".worcs")
+#'   add_testthat()
+#'   usethis::use_test(name = "testme", open = FALSE)
+#'   test_worcs()
+#'   })
+#'
+#' }
+#' @rdname add_testthat
+#' @export
+#' @importFrom usethis with_project use_testthat
+test_worcs <- function(worcs_directory = ".", ...){
+  worcs:::checkworcs(worcs_directory = worcs_directory, iserror = TRUE)
+  usethis::with_project(path = worcs_directory, code = {
+    worcs:::with_cli_try("Loading {.file tests/testthat.R}", {
+      test_path <- file.path("tests", "testthat.R")
+      if(!file.exists(test_path)) stop()
+    })
+    source(test_path)
+  })
+}
