aaf5cf29eb08097bb639c4257e75fc067af70c7c
cjvanlissa <c.j.vanlissa@tilburguniversity.edu>
2026-02-21 17:05:51 +0100

    Add zenodo vignette

--- tests/testthat/test-boilerplate_in_manuscript.R
+++ tests/testthat/test-boilerplate_in_manuscript.R
@@ -1,30 +1,36 @@
 test_that("boilerplate text is inserted", {
-  worcs:::scoped_tempdir({
-    if(requireNamespace("papaja", quietly = TRUE)){
-      add_manuscript(manuscript = "apa6")
-      lnz <- readLines("manuscript/manuscript.Rmd")
-      expect_true(any(grepl("Workflow for Open Reproducible Code in Science", lnz, fixed = TRUE)))
-      expect_true(any(grepl("load_data()", lnz, fixed = TRUE)))
-      file.remove("manuscript/manuscript.Rmd")
-    }
+  withr::with_tempdir({
+    add_manuscript(manuscript = "apa6")
+    lnz <- readLines("manuscript/manuscript.Rmd")
+    expect_true(any(grepl("Workflow for Open Reproducible Code in Science", lnz, fixed = TRUE)))
+    expect_true(any(grepl("load_data()", lnz, fixed = TRUE)))
+    file.remove("manuscript/manuscript.Rmd")
     add_manuscript(manuscript = "github_document")
     lnz <- readLines("manuscript/manuscript.Rmd")
     expect_true(any(grepl("Workflow for Open Reproducible Code in Science", lnz, fixed = TRUE)))
     expect_true(any(grepl("load_data()", lnz, fixed = TRUE)))
     file.remove("manuscript/manuscript.Rmd")
-    if(requireNamespace("rticles", quietly = TRUE)){
     add_manuscript(manuscript = "acm_article")
     lnz <- readLines("manuscript/manuscript.Rmd")
     expect_true(any(grepl("Workflow for Open Reproducible Code in Science", lnz, fixed = TRUE)))
     expect_true(any(grepl("load_data()", lnz, fixed = TRUE)))
     file.remove("manuscript/manuscript.Rmd")
-    }
-    if(requireNamespace("targets", quietly = TRUE) & requireNamespace("tarchetypes", quietly = TRUE)){
     add_manuscript(manuscript = "target_markdown")
-    lnz <- readLines("_targets.Rmd")
+    lnz <- readLines("manuscript/manuscript.Rmd")
     expect_true(any(grepl("Workflow for Open Reproducible Code in Science", lnz, fixed = TRUE)))
-    expect_true(any(grepl("load_data(", lnz, fixed = TRUE)))
-    expect_error(rmarkdown::render("_targets.Rmd", quiet = TRUE), NA)
-    }
+    expect_true(any(grepl("load_data()", lnz, fixed = TRUE)))
+    file.remove("manuscript/manuscript.Rmd")
+  })
+})
+
+
+test_that("boilerplate text is inserted", {
+  withr::with_tempdir({
+    add_manuscript(manuscript = "target_markdown")
+    print(list.files())
+    # lnz <- readLines("manuscript.Rmd")
+    # print(lnz)
+    #expect_true(any(grepl("Workflow for Open Reproducible Code in Science", lnz, fixed = TRUE)))
+    #expect_true(any(grepl("load_data()", lnz, fixed = TRUE)))
   })
 })
